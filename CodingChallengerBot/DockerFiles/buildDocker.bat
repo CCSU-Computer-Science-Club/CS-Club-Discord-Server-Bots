63afdb08d587577084b1c21bae69059f8047b8cf
Battlekeeper <conwaywest@tech-center.com>
2023-11-17 22:17:11 -0500

    Swtich to service.json for easier setup

--- CodingChallengerBot/DockerFiles/buildDocker.bat
+++ CodingChallengerBot/DockerFiles/buildDocker.bat
@@ -6,7 +6,6 @@ docker image rm typescript-validator:latest
 docker image rm javascript-validator:latest
 
 echo "Building new images..."
-docker build --tag python-validator -f python.dockerfile .
-docker build --tag typescript-validator -f typescript.dockerfile .
-docker build --tag javascript-validator -f javascript.dockerfile .
-pause
+docker build --tag python-validator -f CodingChallengerBot\DockerFiles\python.dockerfile .
+docker build --tag typescript-validator -f CodingChallengerBot\DockerFiles\typescript.dockerfile .
+docker build --tag javascript-validator -f CodingChallengerBot\DockerFiles\javascript.dockerfile .
--- CodingChallengerBot/DockerFiles/buildDocker.sh
+++ CodingChallengerBot/DockerFiles/buildDocker.sh
@@ -10,4 +10,3 @@ echo "Building new images..."
 docker build --tag python-validator -f python.dockerfile .
 docker build --tag typescript-validator -f typescript.dockerfile .
 docker build --tag javascript-validator -f javascript.dockerfile .
-pause
