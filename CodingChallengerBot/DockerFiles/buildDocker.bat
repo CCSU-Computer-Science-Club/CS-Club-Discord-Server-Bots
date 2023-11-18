85c706a4bd3437ba0332a61f1d2b82e78d7d87b6
Battlekeeper <conwaywest@tech-center.com>
2023-11-17 21:52:14 -0500

    Update docker build scripts

--- CodingChallengerBot/DockerFiles/buildDocker.bat
+++ CodingChallengerBot/DockerFiles/buildDocker.bat
@@ -1,4 +1,11 @@
-echo "Building docker containers"
+echo "Building docker images..."
+
+echo "Removing old images..."
+docker image rm python-validator:latest
+docker image rm typescript-validator:latest
+docker image rm javascript-validator:latest
+
+echo "Building new images..."
 docker build --tag python-validator -f python.dockerfile .
 docker build --tag typescript-validator -f typescript.dockerfile .
 docker build --tag javascript-validator -f javascript.dockerfile .
